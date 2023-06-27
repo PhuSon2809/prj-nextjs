b5ba88fa10f2dac24e43fdb8cc517873aac52526
Tran Phu Son <phuson2809@gmail.com>
2023-06-27 23:45:19 +0700

    Design navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       <div className="py-4 border-b-[1px]">
         <Container>
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
-            {/* <Logo /> */}
+            <Logo />
             <Search />
             <UserMenu />
           </div>
